d1a1e32522f54fb13da30374f42027ff3c161dc5
XCoolBreezeX <196724073+XCoolBreezeX@users.noreply.github.com>
2025-05-17 02:20:55 +0000

    Nível novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,5 +36,56 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
+    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'}; 
+
+    //Tabuleiro (10x10)
+    int tabuleiro[10][10];
+
+    for (int i = 0; i < 10; i++){
+
+        for (int j = 0; j < 10; j++){
+
+            tabuleiro [i][j] = 0;
+
+        }
+    }
+
+    //Exibir o tabuleiro
+    for (int j = 0; j < 10; j++)
+    {
+        printf("%c ", letras[j]);
+    }
+    printf("\n");
+
+    //Navio na vertical
+    for (int i = 1; i < 4; i++){
+
+        tabuleiro[i][8] = 3;
+
+    }
+    
+    //Navio na horizontal
+    for (int j = 1; j < 4; j++){
+
+        tabuleiro[9][j] = 3;
+
+    }
+
+    //Linhas numeradas 
+    for (int i = 0; i < 10; i++){
+
+        printf("%2d ", i + 1); //número das linhas
+
+        for (int j = 0; j < 10; j++){
+
+            printf("%d ", tabuleiro[i][j]); //Valor do tabuleiro
+
+        }
+
+        printf("\n");
+
+    }
+    
+        return 0;
 }
+
